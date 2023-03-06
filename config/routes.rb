1f1869d754e706e82e32b422cabbcf5188f57634
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-03-06 17:44:26 +0100

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "uikit", to: static("front.html.erb")
+  get "uikit", to: "pages#uikit"
 
-  resources :users, only [:show] do #as profile?
-    resources :trips, only [:new, :create, :update, :destroy] do
-      resources :moments, only [:new, :create] 
-      resources :steps, only [:index, :new, :create]
-    end
+  resources :users, only: [:show]
+
+  resources :trips, only: [:new, :create, :update, :destroy] do
+    resources :moments, only: [:new, :create]
+    resources :steps, only: [:index, :new, :create]
   end
 
-  resources :chatrooms, only [:index, :show, :create, :destroy] do
+  resources :chatrooms, only: [:index, :show, :create, :destroy] do
     resources :messages, only: [:create]
   end
-  resources :steps, only [:edit, :update, :destroy]
 
-
+  resources :steps, only: [:edit, :update, :destroy]
+  resources :moments, only: [:destroy]
 end
