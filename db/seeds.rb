70d226527f2b37e274ee48dba9d8a4db9f2f2f7b
delphine abouab <delphine.abouab@outlook.fr>
2023-03-07 11:59:30 +0100

    seed init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# USERS
+puts "Creating 10 Users"
+
+demo_user = User.new
+demo_user.email
+demo_user.password
+demo_user.username
+demo_user.description
+demo_user.
+
+puts "Done ! (User.all = #{User.all})"
+# TRIPS
+puts "Creating 5 Trips"
+
+
+puts "Done ! (Trip.all= #{Trip.all})"
+
+# STEPS
+puts "Creating  Steps"
+
+
+puts "Done ! (Step.all= #{Step.all})"
+
+# MOMENTS
+puts "Creating  Moments"
+
+
+puts "Done ! (Moment.all= #{Trip.all})"
+
+
+# lesson.photo.attach(io: URI.open("https://source.unsplash.com/random/?#{lesson.category}"), filename: "nes.png", content_type: "image/png")
