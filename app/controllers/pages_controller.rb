8e55ce266ea5d3122ea82a5b83380709cf070157
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-03-07 17:04:46 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.near([current_user.latitude,current_user.longitude],100)
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
+
 end
