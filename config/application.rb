fe3775cc6e8467d4d47ecfc8c60e9683853938ae
Stefan Milosavljevic <stefanva@hawaii.edu>
2023-03-09 16:23:26 +0100

    ui kit and fonts update

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
