025e6a77bed4d3f979bbab906f2b1b85f76dfc99
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-03-10 15:37:31 +0100

    done

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -69,12 +69,3 @@ before_action :set_step, only: [:edit, :update, :destroy]
   end
 
 end
-
-
-    const query = await fetch(
-      `https://api.mapbox.com/directions/v5/mapbox/driving/${start[1]},${start[0]};${end[1]},${end[0]}?steps=true&geometries=geojson&access_token=${mapboxgl.accessToken}`
-    );
-    const json = await query.json();
-    const data = json.routes[0];
-    const route = data.geometry.coordinates;
-    return route
