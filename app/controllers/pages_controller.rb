81af2b323367c6492fef4d1b5c41539614bd8279
delphine abouab <delphine.abouab@outlook.fr>
2023-03-10 11:00:50 +0100

    root home, redirect landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,20 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :landing ]
+  skip_before_action :authenticate_user!, only: [ :landing, :home ]
 
   def home
-    radius = params[:radius].present? ? params[:radius].to_i : 150
-    @users = User.near([current_user.latitude,current_user.longitude],radius)
-    @markers = @users.geocoded.map do |user|
-      {
-        lat: user.latitude,
-        lng: user.longitude,
-        info_window_html: render_to_string(partial: "info_window", locals: {user: user}),
-        marker_html: render_to_string(partial: "marker", locals: {user: user})
-      }
+    if user_signed_in?
+      radius = params[:radius].present? ? params[:radius].to_i : 150
+      @users = User.near([current_user.latitude,current_user.longitude],radius)
+      @markers = @users.geocoded.map do |user|
+        {
+          lat: user.latitude,
+          lng: user.longitude,
+          info_window_html: render_to_string(partial: "info_window", locals: {user: user}),
+          marker_html: render_to_string(partial: "marker", locals: {user: user})
+        }
+      end
+    else
+      redirect_to landing_path unless user_signed_in?
     end
   end
 
