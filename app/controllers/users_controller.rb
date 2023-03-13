fe46968c5dac9e3356eccc1c5fb5e98b8ff6f5ee
delphine abouab <delphine.abouab@outlook.fr>
2023-03-13 16:43:54 +0100

    logout button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,47 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @trips = @user.trips
-    if params[:trip].present?
-      @trip = Trip.find(params[:trip])
+    if user_signed_in?
+        @user = User.find(params[:id])
+        @trips = @user.trips
+        if params[:trip].present?
+          @trip = Trip.find(params[:trip])
+        else
+          @trip = Trip.last
+        end
+        @markers = []
+        @user.trips.each do |trip|
+          steps_list = []
+          trip.steps.order(:date).each do |step|
+            steps_list << {
+              lat: step.latitude,
+              lng: step.longitude,
+              }
+          end
+          @markers << steps_list
+        end
+
+        @current_markers = []
+        @trip.steps.order(:date).each do |step|
+          @current_markers << {
+            lat: step.latitude,
+            lng: step.longitude,
+            }
+        end
+
+        @photos = []
+        @user.trips.each do |trip|
+          steps_list = []
+          trip.moments.each do |moment|
+            steps_list << {
+              lat: moment.latitude,
+              lng: moment.longitude,
+              photo_html: render_to_string(partial: "moments/photo", locals: { moment: moment })
+              }
+          end
+          @photos << steps_list
+        end
     else
-      @trip = Trip.last
-    end
-    @markers = []
-    @user.trips.each do |trip|
-      steps_list = []
-      trip.steps.order(:date).each do |step|
-        steps_list << {
-          lat: step.latitude,
-          lng: step.longitude,
-          }
-      end
-      @markers << steps_list
-    end
-
-    @current_markers = []
-    @trip.steps.order(:date).each do |step|
-      @current_markers << {
-        lat: step.latitude,
-        lng: step.longitude,
-        }
-    end
-
-    @photos = []
-    @user.trips.each do |trip|
-      steps_list = []
-      trip.moments.each do |moment|
-        steps_list << {
-          lat: moment.latitude,
-          lng: moment.longitude,
-          photo_html: render_to_string(partial: "moments/photo", locals: { moment: moment })
-          }
-      end
-      @photos << steps_list
+      redirect_to landing_path
     end
   end
 end
