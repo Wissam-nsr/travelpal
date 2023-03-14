60c9310f059ef8a22023ee1ab041a5084ad15f1a
wissam nasreddine <wissam.nasreddine@hec.edu>
2023-03-14 17:11:49 +0100

    done

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,8 +6,18 @@ class ChatroomsController < ApplicationController
   end
 
   def show
-    @chatrooms = Chatroom.all
-    @chatroom = Chatroom.find(params[:id])
+    if params[:id].present?
+      @chatroom = Chatroom.find(params[:id])
+    elsif Chatroom.last.any?
+      @chatroom = Chatroom.last
+    end
+    @all_chatrooms = Chatroom.all
+    @my_chatrooms = []
+    @all_chatrooms.each do |chatroom|
+      if (chatroom.user_one == current_user || chatroom.user_two == current_user) && chatroom != @chatroom
+        @my_chatrooms << chatroom
+      end
+    end
     @message = Message.new
   end
 
