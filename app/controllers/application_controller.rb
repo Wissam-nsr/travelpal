c25d989c58d1a022e0e155266af1e087ebcb40ea
Stefan Milosavljevic <stefanva@hawaii.edu>
2023-03-15 10:56:07 +0100

    Sliding new moments form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :set_moment
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -8,5 +9,13 @@ class ApplicationController < ActionController::Base
 
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username, :location])
+  end
+
+  private
+
+  def set_moment
+    @moment = Moment.new
+    @trip = current_user.trips.last
+    @moment.trip = current_user.trips.last if current_user
   end
 end
