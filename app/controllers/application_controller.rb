dca52c66d239f22793cd746f123d358a5666d643
delphine abouab <delphine.abouab@outlook.fr>
2023-03-15 18:38:53 +0100

    a third bug fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :set_moment
+  before_action :set_moment, except: :landing
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
