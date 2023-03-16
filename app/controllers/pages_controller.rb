cef16a50c16f8ebd78667442f1912ab3cb81528e
delphine abouab <delphine.abouab@outlook.fr>
2023-03-16 10:05:07 +0100

    should work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,7 +19,6 @@ class PagesController < ApplicationController
   end
 
   def landing
-    flash.now[:notice] = "remote_ip: #{request.location.data["ip"]}, lat: #{request.location.latitude}, long: #{request.location.longitude}"
   end
 
   def map
